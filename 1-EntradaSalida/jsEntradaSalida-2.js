1ec8c9d2c6084f11b1e6ef1a164eebc8025fa521
ulises <57419658+ulises282@users.noreply.github.com>
2020-02-06 11:53:44 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,9 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	//prompt() retorna algo
+	var nombre = prompt("Please enter your name");
+	console.log("bienvenido "+nombre);
+	//alert("bienvenido",nombre)
 }
 
