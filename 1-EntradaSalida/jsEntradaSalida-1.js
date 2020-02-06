961cd749ece6dfbfc600fa6ff420361eee604d99
ulises <57419658+ulises282@users.noreply.github.com>
2020-02-06 11:12:54 -0300

    ejercicio 1
    uso de alert, comentarios, variables

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,17 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
-}
+	var primerPrecio; //definicion 
+	var segundoPrecio;
+	var apellido="nadie"; 
 
+	apellido="perez"; //asignacion
+
+		//es un valor literal si esta entre ""
+	//alert("tal cual");
+		//alert recibe una funcion y la muestra
+		//si el valor no tiene "" es un valor variable 
+	//alert(preciopri);	si la variable no existe no hace nada 
+	alert(apellido);
+	//alert();	sale vacio 
+}
