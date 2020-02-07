a2e10902b7fb093dff826adc669ed169735acd9b
ulises <57419658+ulises282@users.noreply.github.com>
2020-02-07 10:44:09 -0300

    ejercicio 7
    suma, resta, multiplicacion y division

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,69 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaSuma;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
+
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+
+	resultadoDeLaSuma=primerNumero+segundoNumero;
+
+	alert("la suma es: "+resultadoDeLaSuma);
 }
 
 function restar()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaResta;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
+
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+
+	resultadoDeLaResta=primerNumero-segundoNumero;
+
+	alert("la resta es: "+resultadoDeLaResta);
 }
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaMultiplicacion;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
+
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+
+	resultadoDeLaMultiplicacion=primerNumero*segundoNumero;
+
+	alert("la multiplicacion es: "+resultadoDeLaMultiplicacion);
 }
 
 function dividir()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaDivision;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
+
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+
+	resultadoDeLaDivision=primerNumero/segundoNumero;
+
+	alert("la division es: "+resultadoDeLaDivision);
 }
 
