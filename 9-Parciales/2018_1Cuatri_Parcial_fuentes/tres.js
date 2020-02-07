8dede0a7d8026be442989a77895c14bcb8fd6950
ulises <57419658+ulises282@users.noreply.github.com>
2020-02-07 12:24:00 -0300

    ejercicio parcial 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var precioFinal;
+	var descuento
+
+	precio=prompt("ingresar precio: ");
+	precio=parseInt(precio);
+
+	porcentaje=prompt("ingresar porcentaje: ");
+	porcentaje=parseInt(porcentaje);
+
+	descuento=precio*porcentaje/100;
+
+	precioFinal=precio-descuento
+	document.getElementById('')
+
 
 }
