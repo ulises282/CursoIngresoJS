8643a38781207c323ab08e498996a3203ba0ac29
ulises <57419658+ulises282@users.noreply.github.com>
2020-02-07 10:13:26 -0300

    ejercicio 3

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,11 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+	nombre=document.getElementById("elNombre").value;
+	alert(nombre);
+	document.getElementById="juan pablo";
 }
-
+/* document es el html 
+. value es para tomar el valor del cuadro de texto */
 
