0b8a33b5442fec1478af0a096ad6fad5e914ee85
ulises <57419658+ulises282@users.noreply.github.com>
2020-02-07 11:13:56 -0300

    ejercicio 9
    porcentaje

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,16 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var importe;
+	var porcentaje;
+	var resultado;
 
-	
+	importe=document.getElementById('sueldo').value;
+	importe=parseInt(importe);
+
+	porcentaje=importe*0.10;
+
+	resultado=importe+porcentaje;
+
+	document.getElementById('resultado').value=resultado;
 }
