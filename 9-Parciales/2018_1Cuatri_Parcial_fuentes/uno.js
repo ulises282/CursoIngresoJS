d2d7a37f5825cc0b49ae6a5e6dc1d9641bc67f12
ulises <57419658+ulises282@users.noreply.github.com>
2020-02-08 16:23:58 -0300

    ejercicio-parcial1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
+	var anchoDelTriangulo;
+	var largoDelTriangulo;
 
+	anchoDelTriangulo=prompt("ingresa el ancho del triangulo: ");
+	anchoDelTriangulo=parseInt(anchoDelTriangulo);
+
+	largoDelTriangulo=prompt("ingresar el largo del triangulo: ");
+	largoDelTriangulo=parseInt(largoDelTriangulo);
+
+	perimetro=2*(largoDelTriangulo+anchoDelTriangulo);
+
+	alert("el perimetro es: "+perimetro);
 }
