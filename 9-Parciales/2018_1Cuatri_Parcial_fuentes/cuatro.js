9ec370bb17b3fc66f882a787d8f80cd1586fb374
alumno <alumno>
2020-02-14 09:32:45 -0300

    parcial 4

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,42 @@
 function mostrar()
 {
+	var primerNumero;
+	var segundoNumero;
+	var resta;
+	var suma;
+	var concatenado;
+
+	primerNumero=prompt("ingresar el primer numero: ");
+
+	segundoNumero=prompt("ingresar el segundo numero: ");
+
+	if (primerNumero==segundoNumero)
+	{
+		concatenado=primerNumero+segundoNumero;
+		alert("el numero concatenado es: " + concatenado);
+	}
+	else
+	{
+		segundoNumero=parseInt(segundoNumero);
+		primerNumero=parseInt(primerNumero);
+		if (primerNumero>segundoNumero)
+		{
+			resta=primerNumero-segundoNumero;
+			alert("la resta es: " + resta);
+		}
+		else
+		{
+			suma=primerNumero+segundoNumero;
+			if (suma>10)
+			{
+				alert("la suma es " + suma + " y supero el 10");
+			}
+			else
+			{
+				alert("la suma es: " + suma);
+			}
+
+		}
+	}
 
 }
