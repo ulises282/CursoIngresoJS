b57bb6dfd1fafbb747b25b68daa0ade4ec070dab
alumno <alumno>
2020-02-14 09:14:06 -0300

    ejercicios if

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,23 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var random;
+	random = Math.floor(Math.random() * 10) + 1;
+	console.log(random);
+	if (random>8)
+	{
+		alert("EXCELENTE");
+	}
+	else
+	{
+		if(random>3)
+		{
+			alert("APROBÓ");
+		}
+		else
+		{
+			alert("Vamos, la proxima se puede");
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,8 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	Math.floor(Math.random() * 10) + 1;
+	var random;
+	random = Math.floor(Math.random() * 10) + 1;
+	alert("numero random : "+random);
 
 }//FIN DE LA FUNCIÓN
