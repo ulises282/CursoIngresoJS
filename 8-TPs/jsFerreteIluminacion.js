d814867542603df1d0f27bce42297dc12dfd09e3
alumno <alumno>
2020-02-20 10:07:52 -0300

    ferrete iluminacion

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -27,8 +27,61 @@ function CalcularPrecio ()
 
  	subtotal=precioDeLampara*cantidadDeLamparas;
 
- 	descuento=0;
+ 	switch (cantidadDeLamparas)
+ 	{
+ 		case 5:
+ 			switch(marca)
+ 			{
+ 				case "ArgentinaLuz":
+ 					descuento=40;
+ 					break;
 
+ 				default:
+ 					descuento=30;
+ 					break;
+ 			}
+ 			break;
+
+ 		case 4:
+ 			switch(marca)
+ 			{
+ 				case "ArgentinaLuz":
+ 				case "FelipeLamparas":
+ 					descuento=25;
+ 					break;
+
+ 				default:
+ 					descuento=20;
+ 					break;
+ 			}
+ 			break;
+
+ 		case 3:
+ 			switch(marca)
+ 			{
+ 				case "ArgentinaLuz":
+ 					descuento=15;
+ 					break;
+
+ 				case "FelipeLamparas":
+ 					descuento=10;
+ 					break;
+
+ 				default:
+ 					descuento=5;
+ 					break;
+ 			}
+ 			break;
+
+ 		case 2:
+ 		case 1:
+ 			descuento=0
+ 			break;
+
+ 		default:
+ 			descuento=50;
+ 			break;
+ 	}
  	/*if (cantidadDeLamparas>5)
  	{
  		descuento=50;
