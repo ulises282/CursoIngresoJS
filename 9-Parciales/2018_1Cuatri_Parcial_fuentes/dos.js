dfcef4e1d10b55c2f3706967d3fdf121d8543aea
alumno <alumno>
2020-02-28 10:47:07 -0300

    parciales

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,11 +1,69 @@
+/*b)Para la gestión de un hotel,
+ingresar los siguientes datos validados de una reserva
+nombre del huésped
+cantidad de personas 
+cantidad de dia de estadia 
+forma de pago(efectivo , tarjeta o QR)
+
+informar el huésped que trajo más personas en una sola reserva.
+la cantidad de personas que se quedaron más días
+la forma de pago más utilizada.
+el promedio de cantidad de días por reserva*/
 function mostrar()
 {
-  var nombre;
-  var localidad;
+	var nombre;
+	var cantidadPersonas;
+	var estadia;
+	var formaPago;
+	var respuesta;
+	var contador;
+	contador=0;
+	var nombreMasPersonas;
+	var cantidadPersonas;
 
-  nombre=document.getElementById('elNombre').value;
-  localidad=document.getElementById('laLocalidad').value;
-  
-  alert("usted es: " + nombre);
-  alert("y vive en la localidad de: " + localidad)
+	while (respuesta != "no")
+	{
+		contador++;
+		do
+		{
+			nombre=prompt("ingresar el nombre: ");
+		}
+		while (!(isNaN(nombre)));
+
+
+		do
+		{
+			cantidadPersonas=prompt("ingresar la cantidad de personas : ");
+		}
+		while (isNaN(cantidadPersonas)|| cantidadPersonas<1);
+
+		do
+		{
+			estadia=prompt("ingresar la cantidad de dias de estadia : ");
+		}
+		while (isNaN(estadia)|| estadia<1 );
+
+		do 
+		{
+			formaPago=prompt("ingrese forma de pago : (efectivo , tarjeta o QR)");
+		}
+		while (formaPago!="efectivo" && formaPago!="tarjeta" && formaPago!="QR");
+
+		if (contador==1)
+		{
+			nombreMasPersonas=nombre;
+			cantidadPersonas=personas;
+		}
+
+		if (cantidadPersonas<personas)
+		{
+			nombreMasPersonas=nombre;
+			cantidadPersonas=personas;
+		}
+
+
+		respuesta=prompt("desea seguir cargando datos? (no para salir)");
+	}
+
+	alert("el huésped que trajo más personas es: " + nombreMasPersonas + " que trajo : " + cantidadPersonas + " personas ");
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,16 +1,122 @@
 
 function mostrar()
 {
-	var anchoDelTriangulo;
-	var largoDelTriangulo;
+	var nombre;
+	var edad;
+	var sexo;
+	var temperatura;
+	var respuesta;
+	var contador;
+	contador=0;
+	var temperaturaMasAlta;
+	temperaturaMasAlta=0;
+	var nombreMasAlta;
+	nombreMasAlta=0;
+	var temperaturaJoven;
+	temperaturaJoven=0;
+	var temperaturaViejo;
+	temperaturaViejo=0;
+	var edadViejo;
+	edadViejo=0;
+	var edadJoven;
+	edadJoven=0;
+	var edadTotal;
+	edadTotal=0;
+	var edadMujeres;
+	edadMujeres=0;
+	var promedioTotal;
+	promedioTotal=0;
+	var promedioMujeres;
+	promedioMujeres=0;
+	var contadorMujeres;
+	contadorMujeres=0;
 
-	anchoDelTriangulo=prompt("ingresa el ancho del triangulo: ");
-	anchoDelTriangulo=parseInt(anchoDelTriangulo);
+	while(respuesta!="no")
+	{
+		contador++;
+		do
+		{
+			nombre=prompt("ingresar nombre : ");
+		}
+		while(!(isNaN(nombre)));
 
-	largoDelTriangulo=prompt("ingresar el largo del triangulo: ");
-	largoDelTriangulo=parseInt(largoDelTriangulo);
+		do
+		{
+			edad=prompt("ingresar edad :");
+			edad=parseInt(edad);
+		}
+		while(isNaN(edad)||edad>110||edad<1);
 
-	perimetro=2*(largoDelTriangulo+anchoDelTriangulo);
+		do 
+		{
+			sexo=prompt("ingresar sexo (f o m): ");
+		}
+		while(sexo!="m" && sexo !="f");
 
-	alert("el perimetro es: "+perimetro);
+		do
+		{
+			temperatura=prompt("ingresar temperatura : ");
+		}
+		while(isNaN(temperatura)||temperatura>45||temperatura<10);
+
+		respuesta=prompt("desea seguir cargando datos? (no para salir)");
+
+		if (contador==1)
+		{
+			temperaturaMasAlta=temperatura;
+			nombreMasAlta=nombre;
+			temperaturaViejo=temperatura;
+			edadViejo=edad;
+			temperaturaJoven=temperatura;
+			edadJoven=edad;
+		}
+		if (temperaturaMasAlta<temperatura)
+		{
+			temperaturaMasAlta=temperatura;
+			nombreMasAlta=nombre;
+		}
+		if (temperatura>38)
+		{
+			if (edad<13)
+			{
+				alert(" riesgo con : " + nombre + " edad : " + edad + " la temperatura es de: " + temperatura);
+			}
+			else
+			{
+				if (edad>60)
+				{
+					alert(" riesgo con : " + nombre + " edad : " + edad + " la temperatura es de: " + temperatura);
+				}
+			}
+		}
+		if (edadViejo<edad)
+		{
+			temperaturaViejo=temperatura;
+			edadViejo=edad;
+		}
+		else
+		{
+			if (edadJoven>edad)
+			{
+				temperaturaJoven=temperatura;
+				edadJoven=edad;
+			}
+		}
+		edadTotal=edadTotal+edad;
+		if (sexo=="f")
+		{
+			contadorMujeres++;
+			edadMujeres=edadMujeres+edad;
+		}
+
+	}
+	promedioTotal=edadTotal/contador;
+	promedioMujeres=edadMujeres/contadorMujeres;
+	alert("la temperatura mas alta es de: " + nombreMasAlta + " y es de : " + temperaturaMasAlta + "ºC");
+	alert("la temperatura de la persona mas vieja es de : " + temperaturaViejo + " y teiene : " + edadViejo + " años ");
+	alert("la temperatura de la persona mas joven es de : " + temperaturaJoven + " y teiene : " + edadJoven
+	 + " años ");
+	alert("el promedio total de edad es de : " + promedioTotal);
+	alert("el promedio de edad de la mujeres es de: " + promedioMujeres);
+
 }
